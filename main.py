0cc67ada980c9a7dafe9d329f13bf6605d9de505
Myke Atkinson <ploe@hotmail.co.uk>
2018-11-10 11:30:08 +0000

    [WIP] Started work on MySQL Driver

--- main.py
+++ main.py
@@ -5,9 +5,30 @@ import json
 from flask import Flask
 app = Flask(__name__)
 
+import MySQLdb
+
 sample = {
 	"hello": "world", "hi": "hello"
 }
+
+class MysqlDriver():
+	def __init__(self):
+		self.db = MySQLdb.connect(
+  			host="localhost",
+ 			user="root",
+  			passwd="+zQx57?4$9",
+  			db="crud"
+		)
+
+		self.cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
+
+	def __del__(self):
+		self.db.close()
+
+	def query(self, op, params):
+		self.cursor.execute(op, params)
+		return self.cursor.fetchall()
+
 
 with open("sample.json") as data:
 	sample = json.load(data)
@@ -20,7 +41,14 @@ def root():
 def list(resource):
 	return resource, 200, {'Content-Type': 'application/json'}
 
-@app.route("/<string:resource>/<tag>")
+@app.route("/<string:resource>/<string:tag>")
 def item(resource, tag):
+	db = MysqlDriver()
+
 	endpoint = sample[resource]
-	return json.dumps(endpoint), 200, {'Content-Type': 'application/json'}
+
+	query = endpoint['item']['get']['query']
+	print(query)
+	item = db.query(query['op'], (52,))
+
+	return json.dumps(item), 200, {'Content-Type': 'application/json'}
