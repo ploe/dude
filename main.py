1b99200e1db65161820dfe3181c760c6fbec1836
Myke Atkinson <ploe@hotmail.co.uk>
2018-11-12 13:17:56 +0000

    [WIP]: limit transform added

--- main.py
+++ main.py
@@ -26,25 +26,43 @@ class MysqlDriver():
 		self.cursor.close()
 		self.db.close()
 
-	def jinja(self, param):
-		t = Template(param)
-		return t.render(uri=self.uri, url=self.url)
-
-	def jinja_params(self, src):
-		params = []
-		for param in src:
-			params.append( self.jinja(param) )
-
-		return params
-
-	def member_get(self, query):
-		params = self.jinja_params(query['params'])
+	def read(self, query):
+		params = self.jinja_params(query)
 
 		self.cursor.execute(query['op'], params)
 		return self.cursor.fetchall()
 
+	def write(self, query):
+		params = self.jinja_params(query)
+
+		self.cursor.execute(query['op'], params)
+		self.db.commit()
+
+		return True
+
+	def jinja(self, param):
+		t = Template(param)
+		return t.render(uri=self.uri, url=self.url)
+
+	def jinja_params(self, query):
+		src = query.get('params', None)
+
+		params = []
+		if src != None:
+			for param in src:
+				params.append( self.jinja(param) )
+
+		return params
+
+	def member_get(self, query):
+		return self.read(query)
+
+	def collection_get(self, query):
+		return self.read(query)
+
 	def collection_post(self, query):
-		params = self.jinja_params(query['params'])
+		params = self.jinja_params(query)
+
 		self.cursor.execute(query['op'], params)
 		self.db.commit()
 
@@ -59,40 +77,11 @@ def ok(output):
 	return respond(output, 200)
 
 def respond(output, code):
-	return jsonify(output), 200, {'Content-Type': 'application/json'}
+	return jsonify(output), code, {'Content-Type': 'application/json'}
 
-@app.before_request
-def before_request():
-	url = {}
-	for key, value in request.args.iteritems():
-		url[key] = value
-
-	g.url = url
-	
-@app.route('/')
-def root():
-	return "{'hi': 'hello'}", 200,  {'Content-Type': 'application/json'}
-
-@app.route("/<string:collection>", strict_slashes=False)
-def collection_get(collection):
-	return collection, 200, {'Content-Type': 'application/json'}
-
-@app.route("/<string:collection>/<string:member>", methods=['GET'], strict_slashes=False)
-def member_get(collection, member):
-	db = MysqlDriver({ 'collection': collection, 'member': member }, g.url)
-
-	endpoint = domain[collection]['member']['get']
-
-	query = endpoint['query']
-	results = db.member_get(query)
-
-	if len(results) > 1:
-		abort(500)
-
-	this = results[0]
-
+def render_member(this, endpoint):
 	data = endpoint['data']
-	types = endpoint['types']
+	types = endpoint.get('types', {})
 
 	output = {}
 	for key, raw in data.iteritems():
@@ -108,6 +97,65 @@ def member_get(collection, member):
 			output[key] = float(value)
 		else:
 			output[key] = value
+	return output
+
+@app.before_request
+def before_request():
+	url = {}
+	for key, value in request.args.iteritems():
+		url[key] = value
+
+	g.url = url
+
+@app.route('/')
+def root():
+	return "{'hi': 'hello'}", 200,  {'Content-Type': 'application/json'}
+
+@app.route("/<string:collection>", methods=['GET'], strict_slashes=False)
+def collection_get(collection): 
+	db = MysqlDriver({ 'collection': collection }, g.url)
+	endpoint = domain[collection]['collection']['get']
+
+	results = db.collection_get(endpoint['query'])
+
+	output = []
+	for this in results:
+		output.append( render_member(this, endpoint) )
+
+	transforms = endpoint.get('transforms', [])
+	for transform in transforms:
+		op = transform.get('op', '')
+
+		value = None
+		if op in ('limit', 'sort'):
+			t = Template(transform[op])
+			value = t.render(url=g.url)
+
+		if op == 'limit':
+			try:
+				limit = int(value)
+				output = output[:limit]
+
+			except ValueError:
+				pass
+	
+	return ok(output)
+
+@app.route("/<string:collection>/<string:member>", methods=['GET'], strict_slashes=False)
+def member_get(collection, member):
+	db = MysqlDriver({ 'collection': collection, 'member': member }, g.url)
+
+	endpoint = domain[collection]['member']['get']
+
+	query = endpoint['query']
+	results = db.member_get(query)
+
+	if len(results) > 1:
+		abort(500)
+
+	this = results[0]
+
+	output = render_member(this, endpoint)
 	
 	return ok(output)
 
@@ -121,7 +169,6 @@ def collection_post(collection):
 	db.collection_post(query)
 
 	return ok({'posted': True})
-
 
 @app.errorhandler(404)
 def not_found(error):
