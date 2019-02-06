abac471f29a4954e3a2c8d1d9d09916b46e2e398
Myke Atkinson <ploe@hotmail.co.uk>
2019-02-06 16:45:56 +0000

    MySQLDriver finished, with unittests. :)

--- MySQLDriver.py
+++ MySQLDriver.py
@@ -3,22 +3,27 @@ import MySQLdb
 class MySQLDriver:
     def __init__(self, src):
         self.db = MySQLdb.connect(**src)
-
         self.cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
 
     def __del__(self):
         self.cursor.close()
         self.db.close()
 
-    def get(self, query):
+    def read(self, query):
             try:
                 self.cursor.execute(query['op'], query['params'])
-            except:
+            except Exception as e:
                 return None
 
             return self.cursor.fetchall()
 
-    def post(self, query):
+    def create(self, query):
+        return self.write(query)
+
+    def update(self, query):
+        return self.write(query)
+
+    def write(self, query):
         try:
             self.cursor.execute(query['op'], query['params'])
             self.db.commit()
@@ -27,3 +32,11 @@ class MySQLDriver:
 
         return self.cursor.lastrowid
 
+    def delete(self, query):
+        try:
+            self.cursor.execute(query['op'], query['params'])
+            self.db.commit()
+        except:
+            return None
+
+        return self.cursor.rowcount
--- test_MySQLDriver.py
+++ test_MySQLDriver.py
@@ -8,17 +8,19 @@ class MySQLDriverTestCase(unittest.TestCase):
     def setUp(self):
         src = {
             'host': "127.0.0.1",
-            'user': "root",
+            'user': "pont",
             'passwd': "+zQx57?4$9",
         }
         
         self.mysql = MySQLDriver(src)
         self.mysql.cursor.execute("""
                 CREATE DATABASE pont;
-                CREATE TABLE pont.driver_test (
+                CREATE TABLE pont.test_MySQLDriver (
+                    id int NOT NULL AUTO_INCREMENT,
                     name VARCHAR(255),
                     age INT,
-                    created DATETIME
+                    dob DATETIME,
+                    PRIMARY KEY (id)
                 );
         """)
 
@@ -26,40 +28,78 @@ class MySQLDriverTestCase(unittest.TestCase):
     def tearDown(self):
         self.mysql.cursor.execute("DROP DATABASE pont")
 
-    def created(self):
+
+    def dob(self):
         return datetime.strptime('2018-10-02 9:13:37', '%Y-%m-%d %H:%M:%S')
 
 
-    def get(self):
+    def create(self):
         query = {
-            'op': "SELECT * FROM pont.driver_test",
+            'op': "INSERT INTO pont.test_MySQLDriver (name, age, dob) VALUES (%s, %s, %s)",
+            'params': ["Myke", 31, self.dob()],
+        }
+
+        return self.mysql.create(query)
+
+
+    def read(self):
+        query = {
+            'op': "SELECT * FROM pont.test_MySQLDriver",
             'params': []
         }
 
-        return self.mysql.get(query)
+        return self.mysql.read(query)
 
 
-    def post(self):
+    def update(self, rowid):
         query = {
-            'op': "INSERT INTO pont.driver_test (name, age, created) VALUES (%s, %s, %s)",
-            'params': ["Myke", 31, self.created()],
+            'op': "UPDATE pont.test_MySQLDriver SET name=%s, age=%s WHERE id=%s",
+            'params': ['George', 34, rowid],
         }
 
-        return self.mysql.post(query)
+        return self.mysql.update(query)
 
 
-    def test_post(self):
-        self.assertNotEqual(self.post(), None)
+    def delete(self, rowid):
+        query = {
+            'op': "DELETE FROM pont.test_MySQLDriver WHERE id=%s",
+            'params': [rowid],
+        }
+
+        return self.mysql.delete(query)
 
 
-    def test_get(self):
-        self.post()
-        rows = self.get()
+    def test_create(self):
+        self.assertNotEqual(self.create(), None)
+
+
+    def test_read(self):
+        self.create()
+        rows = self.read()
 
         self.assertNotEqual(rows, None)
 
         for row in rows:
             self.assertEqual(row['name'], "Myke")
             self.assertEqual(row['age'], 31)
-            self.assertEqual(row['created'], self.created())
+            self.assertEqual(row['dob'], self.dob())
+
+
+    def test_update(self):
+        rowid = self.create()
+        self.update(rowid)
+
+        rows = self.read()
+        for row in rows:
+            self.assertEqual(row['id'], rowid)
+            self.assertEqual(row['name'], "George")
+            self.assertEqual(row['age'], 34)
+            self.assertEqual(row['dob'], self.dob())
+
+
+    def test_delete(self):
+        rowid = self.create()
+
+        self.assertTrue(self.delete(rowid))
+
 
