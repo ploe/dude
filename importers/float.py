e971554b587b42b47ca2236aa65e630d602edac7
Myke Atkinson <ploe@hotmail.co.uk>
2019-06-20 20:33:30 +0100

    [WIP] float type added

---
+++ importers/float.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+from importers.base import TypeImporter as base
+
+class TypeImporter(base):
+	def init_type_importer(self, component):
+		self.type = 'float'
+		self.load_component(component, [], 'reject')
+
+		self.value_to_float()
+
+	def value_to_float(self):
+		try:
+			self.value = float(self.value)
+		except ValueError:
+			err = "{}['{}'] ({}): invalid float".format(
+				self.key,
+				self.rule, 
+				self.type, 
+				self.value)
+
+			self.errors.append(err)
+
+		return self.has_errors()
+
+
+	def valid(self):
+		self.validate_reject()
+		return not self.has_errors()
