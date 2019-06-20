b8adc2d742b645c5b4bc0a014f8a3c3fd3108c2e
Myke Atkinson <ploe@hotmail.co.uk>
2019-06-20 18:58:15 +0100

    [WIP] tests/importers/test_float added

---
+++ tests/importers/test_float.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+from importers.float import TypeImporter
+
+import unittest
+
+EXPECTED_VALUE=3.14159
+
+class ImporterBooleanTestCase(unittest.TestCase):
+	def test_float_as_float(self):
+		key = 'unittest'
+		rule = 'test_float_as_float'
+		value = 3.14159
+		component = {
+			'type': 'float'
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertTrue(type_importer.valid())
+		self.assertTrue(type_importer.value, EXPECTED_VALUE)
+			
+
+	def test_float_as_string(self):
+		key = 'unittest'
+		rule = 'test_float_type'
+		value = "{}".format(EXPECTED_VALUE)
+		component = {
+			'type': 'float'
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertTrue(type_importer.valid())
+		self.assertTrue(type_importer.value, EXPECTED_VALUE)
+
+		value = '3.14pi'
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertFalse(type_importer.valid())
+
+	def test_float_reject(self):
+		key = 'unittest'
+		rule = 'test_boolean_reject'
+		value = EXPECTED_VALUE
+		component = {
+			'type': 'float',
+			'reject': ["this == {}".format(EXPECTED_VALUE)]
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertFalse(type_importer.valid())
+
