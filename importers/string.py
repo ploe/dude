57c2a5c65a9002b6a3db9e74e2888a32a5774a5c
Myke Atkinson <ploe@hotmail.co.uk>
2019-06-24 18:14:37 +0100

    String added! =)

---
+++ importers/string.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+from importers.base import TypeImporter as base
+
+class TypeImporter(base):
+	def init_type_importer(self, component):
+		self.type = 'string'
+		self.load_component(component, [], 'reject')
+
+		self.value_to_string()
+
+	def value_to_string(self):
+		try:
+			self.value = str(self.value)
+		except ValueError:
+			self.append_error("'{}' invalid string", self.value)
+
+		except TypeError:
+			self.append_error("'{}' invalid type, should be string", self.value)
+
+		return self.has_errors()
+
+
+	def valid(self):
+		if not self.has_errors():
+			self.validate_reject()
+
+		return not self.has_errors()
+
+
---
+++ tests/importers/test_string.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+from importers.string import TypeImporter
+
+import unittest
+
+EXPECTED_VALUE='hello, world'
+
+class ImporterStringTestCase(unittest.TestCase):
+	def test_string_as_string(self):
+		key = 'unittest'
+		rule = 'test_string_as_string'
+		value = 'hello'
+		component = {
+			'type': 'string'
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertTrue(type_importer.valid())
+		self.assertTrue(type_importer.value, EXPECTED_VALUE)
+			
+
+	def test_string_as_integer(self):
+		key = 'unittest'
+		rule = 'test_string_type'
+		value = 13
+		component = {
+			'type': 'string'
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertTrue(type_importer.valid())
+		self.assertTrue(type_importer.value, EXPECTED_VALUE)
+
+	def test_string_reject(self):
+		key = 'unittest'
+		rule = 'test_string_reject'
+		value = EXPECTED_VALUE
+		component = {
+			'type': 'string',
+			'reject': ['this == "hello, world"']
+		}
+
+		type_importer = TypeImporter(key, rule, value, component)
+		self.assertFalse(type_importer.valid())
+
