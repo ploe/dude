0c9768683c205710c1d7cac24c770f8b1ffeeec5
Myke Atkinson <ploe@hotmail.co.uk>
2019-07-16 09:18:18 +0100

    [WIP] pylint and yapf added to requirements

--- importers/base.py
+++ importers/base.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
-"""This module contains the base TypeImporter"""
+"""This module contains the base TypeImporter."""
 
 
 class TypeImporter():
-    """The base TypeImporter is the TypeImporter all others should inherit from"""
+    """The base TypeImporter is the TypeImporter all child classes should inherit from."""
     def __init__(self, key, rule, value, component):
         self.errors = []
         self.key = key
@@ -13,19 +13,25 @@ class TypeImporter():
         self.init_type_importer(component)
 
     def init_type_importer(self, component):
+        """Needs defining in each child TypeImporter.
+        Runs the child class specific init functions"""
         raise NotImplementedError
 
     def has_errors(self):
+        """Returns True if the TypeImporter generated any errors"""
         return bool(self.errors)
 
     def valid(self):
+        """Needs defining in each child class. Should return True if the data is valid"""
         raise NotImplementedError
 
     def load_component(self, component, default, *args):
+        """Used to unpack the component in to the TypeImporter class"""
         for key in args:
             setattr(self, key, component.get(key, default))
 
     def append_error(self, prompt, *args):
+        """Add a new error message to errors"""
         msg = prompt.format(*args)
         err = "{}['{}'] ({}): {}".format(
             self.key,
@@ -37,6 +43,8 @@ class TypeImporter():
         self.errors.append(err)
 
     def validate_reject(self):
+        """Run the eval on the value imported.
+		This is development only, and should be considered deprecated"""
         for reject in self.reject:
             local = {'this': self.value}
 
