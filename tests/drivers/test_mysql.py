ae2849243ef0baa9da06fc9bc02a70138db95337
Myke Atkinson <ploe@hotmail.co.uk>
2019-08-13 14:51:33 +0100

    [WIP] test_get added to tests/drivers/test_mysql.py

--- tests/drivers/test_mysql.py
+++ tests/drivers/test_mysql.py
@@ -20,12 +20,7 @@ class DriverTestCase(unittest.TestCase):
         self.driver.cursor.execute(
             "CREATE DATABASE IF NOT EXISTS dude_unittests;")
 
-    def get_imported_data(self):
-        with open('./examples/tests/data.yml') as f:
-            return yaml.load(f, Loader=yaml.Loader)
-
-    def test_post(self):
-        table = 'dude_unittests.test_post'
+    def create_table(self, table):
         schema = """
             CREATE TABLE IF NOT EXISTS {} (
                 id int NOT NULL AUTO_INCREMENT,
@@ -38,21 +33,72 @@ class DriverTestCase(unittest.TestCase):
         """.format(table)
         self.driver.cursor.execute(schema)
 
-        imported = {'data': self.get_imported_data()}
+    def delete_table(self, table):
+        query = "DROP TABLE IF EXISTS {}".format(table)
+        self.driver.cursor.execute(query)
 
+    def insert_into_table(self, table):
+        imported = {'data': self.get_imported_data()}
         query = {
             'op':
             "INSERT INTO {} (firstname, lastname, hobby, lucky_number) VALUES (%s, %s, %s, %s);"
             .format(table),
             'params': [
-                'data.firstname', 'data.lastname', 'hi helo',
+                'data.firstname', 'data.lastname', 'data.hobby',
                 'data.lucky_number'
             ]
         }
-        rows = self.driver.post(imported, query)
+        return self.driver.post(imported, query)
 
-        query = "DROP TABLE IF EXISTS {}".format(table)
-        self.driver.cursor.execute(query)
+    def get_imported_data(self):
+        with open('./examples/tests/data.yml') as f:
+            return yaml.load(f, Loader=yaml.Loader)
+
+    def test_post(self):
+        table = 'dude_unittests.test_post'
+        self.create_table(table)
+
+        rows = self.insert_into_table(table)
+
+        self.delete_table(table)
 
         for row in rows:
             self.assertTrue(row.get('lastrowid', False))
+
+    def test_get(self):
+        table = 'dude_unittests.test_get'
+        self.create_table(table)
+
+        inserts = self.insert_into_table(table)
+
+        firstname = 'Noah'
+        lucky_number = 7
+
+        data = []
+        for insert in inserts:
+            if (insert['firstname'] == firstname) and (
+                    insert['lucky_number'] == lucky_number):
+                insert['id'] = insert.pop('lastrowid')
+                data.append(insert)
+
+        imported = {
+            'args': {
+                'firstname': firstname,
+                'lucky_number': lucky_number,
+            }
+        }
+        query = {
+            'op':
+            "SELECT * FROM {} WHERE firstname=%s and lucky_number=%s".format(
+                table),
+            'params': ['args.firstname', 'args.lucky_number'],
+        }
+
+        rows = self.driver.get(imported, query)
+        for row in rows:
+            datum = data.pop()
+
+            for key in row:
+                self.assertEqual(row[key], datum[key])
+
+        self.delete_table(table)
